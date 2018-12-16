f5b8902cf9868f694bc4075b29398fe542cc8024
Ciocirlan Stefan-Dan <stefan_dan@xn--ciocrlan-o2a.ro>
2018-12-16 20:37:22 +0100

    patch

--- client/js/chat.js
+++ client/js/chat.js
@@ -67,6 +67,7 @@ $(".mytext").on("keydown", function(e){
 });
 
 function chat_send_message_button_click() {
+    console.log("a trimis mesaj");
     $(".mytext").trigger({type: 'keydown', which: 13, keyCode: 13});
 }
 
