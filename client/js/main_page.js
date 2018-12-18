f4d1ff0e2dba5012369d8816dda681e8e35a69f7
Ciocirlan Stefan-Dan <stefan_dan@xn--ciocrlan-o2a.ro>
2018-12-18 14:14:56 +0100

    patch

--- client/js/main_page.js
+++ client/js/main_page.js
@@ -267,8 +267,8 @@ function main_page_init() {
 		divID: 'main_page_rgb_robot_div',
 		host: window.location.hostname,
         port: 8000,
-		width: (x2-15)/2,
-        height: ((y2-10)*3)/10,
+		width: 100,
+        height: 100,
         quality: 40,
 		// topic: '/kinect2/k2_rgb_sd/image'
 	    topic: '/xtion/rgb/image_raw'
