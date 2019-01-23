839e8b1d938a8cf5f98f80a2872f6629a4ab9669
Ciocirlan Stefan-Dan <stefan_dan@xn--ciocrlan-o2a.ro>
2019-01-23 13:36:29 +0100

    patch

--- client/js/main_page.js
+++ client/js/main_page.js
@@ -320,9 +320,9 @@ function main_page_init() {
         "showControls": false
     });
 
-    document.getElementById("main_page_rgb_room_img_center").src = "/img/cond_" + main_page_cond3 + "/room_center.jpeg";
-    document.getElementById("main_page_rgb_room_img_1").src = "/img/cond_" + main_page_cond3 + "/room_center.jpeg";
-    document.getElementById("main_page_rgb_room_img_2").src = "/img/cond_" + main_page_cond3 + "/room_center.jpeg";
+    document.getElementById("main_page_rgb_room_img_center").src = "/img/cond_" + main_page_cond3 + "/room_center";
+    document.getElementById("main_page_rgb_room_img_1").src = "/img/cond_" + main_page_cond3 + "/room_center";
+    document.getElementById("main_page_rgb_room_img_2").src = "/img/cond_" + main_page_cond3 + "/room_center";
 
     var video = null;
     var source = null;
