39f410a77b427a20fb5f98097b397764c0afdc39
Ciocirlan Stefan-Dan <stefan_dan@xn--ciocrlan-o2a.ro>
2019-01-23 03:22:12 +0100

    patch

--- client/js/tiago_info_page.js
+++ client/js/tiago_info_page.js
@@ -1,5 +1,7 @@
 
+var tiagoInfoIndex = null;
 function tiago_info_page_enter() {
+    tiagoInfoIndex = 0;
     document.getElementById("tiago_info_page").hidden = false;
     experiment_events[experiment_index] = {
         'dateString' : new Date().toJSON(),
@@ -10,11 +12,13 @@ function tiago_info_page_enter() {
 }
 
 function tiago_info_page_exit() {
+    tiagoInfoIndex = 0;
     document.getElementById("tiago_info_page").hidden = true;
 }
 
 function tiago_info_page_next_btn_click() {
-    if(tiagoInfoIndex == tiagoInfoMessages.length) {
+    tiagoInfoIndex = tiagoInfoIndex + 1;
+    if(tiagoInfoIndex == 1) {
         changeState("task_info_page");
     } else {
         experiment_events[experiment_index] = {
